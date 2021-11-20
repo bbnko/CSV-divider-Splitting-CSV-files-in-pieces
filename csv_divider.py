4c93f2d9ecf30c0351c521d888eb0cf6e51a1ca9
BGDN <94234954+bbnko@users.noreply.github.com>
2021-11-20 16:33:05 +0100

    Delete csv_divider.py

--- csv_divider.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-chunk_size = 700000
-batch_no = 1
-
-for chunk in pd.read_csv('E:\Bogdan\Downloads\Cross_Coding_Details_Report_2399115_16062020_120456.CSV', chunksize=chunk_size):
-   chunk.to_csv('Cross_Coding_Details_Report_2399115_16062020_120456' +str(batch_no) +'.csv', index=False)
-   batch_no +=1
